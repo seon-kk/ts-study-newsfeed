7141f74a08fe074bb34b332f347ff0eabdc41bed
SeonK <seon_kk@naver.com>
2021-09-17 11:12:15 +0900

    mocha, supertest api 테스트해보기

--- src/test/test.ts
+++ src/test/test.ts
@@ -1,29 +1,35 @@
 import { expect } from 'chai';
 import { agent as request } from 'supertest';
 import app from '../app';
+import { createConnection } from "typeorm";
 
-describe("Index Test", () => {
-    it('학교 등록 테스트', async function () {
+
+
+
+describe("api test", () => {
+
+    /*it('학교 등록 테스트', async function () {
         const res = await request(app)
             .post('/school')
             .send(schoolPost);
         expect(res.status).to.equal(200);
-    });
-
-
-    it('학교 등록 테스트', async function () {
-        const res = await request(app)
-            .post('/school')
-            .send(schoolPost);
-        expect(res.status).to.equal(200);
-    });
-
+    });*/
     it('학교 소식 등록 테스트', async function () {
+
         const res = await request(app)
-            .post('/posts')
+            .post('/post')
             .send(posts);
-        expect(res.status).to.equal(200);
+        expect(res.status).to.equal(400);
+
     });
+
+    /*it('구독 등록 테스트', async function () {
+        const res = await request(app)
+            .post('/subs')
+            .send(subs);
+        expect(res.status).to.equal(200);
+    });*/
+
 });
 
 
@@ -32,14 +38,17 @@ const schoolPost = {
     region: '강원',
     adminIdx: 4
 }
-
 const posts = {
-    adminIdx: 4,
-    subject: '소식 테스트 4',
-    content: '소식 테스트 4의 본문입니다.'
+    adminIdx: 99,
+    subject: '버섯초등학교 소식 2',
+    content: '버섯초등학교 소식 2의 본문입니다.'
+};
+
+
+const subs = {
+    studentIdx: 1,
+    schoolIdx: 10
 }
-
-
 
 
 
